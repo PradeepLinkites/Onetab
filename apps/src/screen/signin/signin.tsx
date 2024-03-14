334beec1200e3bc1b3ba7e3b3c0d5cc307ae132e
Pradeep Choudhary <pradeep@linkites.com>
2024-03-14 14:16:51 +0530

    feat(ui): add google signin

--- apps/src/screen/signin/signin.tsx
+++ apps/src/screen/signin/signin.tsx
@@ -5,19 +5,21 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
-  ImageBackground,
   useWindowDimensions,
-  Pressable,
+  SafeAreaView,
 } from "react-native";
-
-import { Logo, ArrowBlue } from "../../assets";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from "@react-native-google-signin/google-signin";
+import { Logo } from "../../assets";
 import { RootRoutes } from "../../navigation/routes";
 import { useDispatch, useSelector } from "react-redux";
 import { Dispatch } from "redux";
 import { styles } from "./styles";
 import { authStoreActions, login } from "../../../store";
 import { useToast } from "react-native-toast-notifications";
-import { AnimatedImageBackGround } from "../animatedImageBackground";
 
 export const SignIn = () => {
   const dispatch = useDispatch<Dispatch<any>>();
@@ -25,11 +27,13 @@ export const SignIn = () => {
   const [emailAddress, setEmailAddress] = useState<string>();
   const navigation = useNavigation<any>();
   const { width, height } = useWindowDimensions();
+  const [user, setUser] = useState({});
   const { loginData, loginStatus } = useSelector((state: any) => ({
     loginData: state.authStore.loginData,
     loginStatus: state.authStore.loginStatus,
   }));
   const apiUp = true;
+
   useEffect(() => {
     if (loginData.data !== undefined) {
       dispatch(authStoreActions.setLoadingStatus("not loaded"));
@@ -41,6 +45,34 @@ export const SignIn = () => {
     }
     return;
   }, [loginData]);
+
+  GoogleSignin.configure({
+    scopes: ["https://www.googleapis.com/auth/drive.readonly"], // what API you want to access on behalf of the user, default is email and profile
+    webClientId:
+      "626734696093-c1lnr3hmaupofd742el8aqiaajvutd2q.apps.googleusercontent.com", // client ID of type WEB for your server. Required to get the idToken on the user object, and for offline access.
+    iosClientId:
+      "626734696093-t9q814l90r10g2ko706uf0atpras8br2.apps.googleusercontent.com",
+    offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
+  });
+
+  const _signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log("userInfo==>>", userInfo);
+      // setState({ userInfo });
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
 
   const validation = (email: string | undefined) => {
     if (emailAddress !== "" && emailAddress !== undefined) {
@@ -62,29 +94,31 @@ export const SignIn = () => {
 
   const signIn = async (email) => {
     apiUp
-      ? dispatch(login({ email: email , isMobile: true }))
+      ? dispatch(login({ email: email, isMobile: true }))
       : navigation.navigate(RootRoutes.Verification_Screen);
   };
 
   return (
-    <View style={styles.container}>
-      <AnimatedImageBackGround />
-      <View style={styles.logo_Style}>
-        <Logo />
-      </View>
-      <View
-        style={{
-          justifyContent: "center",
-          alignItems: "center",
-          width: "100%",
-          marginTop: '10%',
-          flex: 6,
-        }}
-      >
-        <Text style={styles.Sign_In_To_ConfigueeX_Text_Style} numberOfLines={1}>
-          Sign In to Wynn
-        </Text>
-        <View style={styles.shadowProp}>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.logo_Style}>
+          <Logo />
+        </View>
+        <View style={styles.middlecontainer}>
+          <Text
+            style={styles.signintext}
+            numberOfLines={1}
+            allowFontScaling={false}
+          >
+            Sign In to Onetab
+          </Text>
+          <GoogleSigninButton
+            style={{ marginTop: 15, height: 50 }}
+            size={GoogleSigninButton.Size.Wide}
+            onPress={_signIn}
+            // disabled={this.state.isSigninInProgress}
+          />
+
           <TextInput
             onChangeText={(e: string) => {
               setEmailAddress(e.toLowerCase());
@@ -97,37 +131,18 @@ export const SignIn = () => {
             value={emailAddress}
             autoCapitalize="none"
           />
+          <TouchableOpacity
+            style={styles.signUpButton}
+            onPress={() => {
+              validation(emailAddress);
+            }}
+          >
+            <Text allowFontScaling={false} style={styles.testSignUp}>
+              Sign In
+            </Text>
+          </TouchableOpacity>
         </View>
-
-        <TouchableOpacity
-          style={styles.signUpButton}
-          onPress={() => {
-            validation(emailAddress);
-          }}
-        >
-          <Text allowFontScaling={false} style={styles.testSignUp}>
-            Sign In
-          </Text>
-        </TouchableOpacity>
-        <View
-          style={{
-            flexDirection: "row",
-            marginHorizontal: "13%",
-            marginTop: "20%",
-            alignItems: "center",
-          }}
-        ></View>
       </View>
-      <View style={styles.bottom_Style}>
-        <Text style={styles.need_Configure_X_Text_Style}>New to Wynn?</Text>
-        <Pressable
-          onPress={() => navigation.navigate(RootRoutes.Sign_Up_Screen)}
-          style={styles.createAccountStyle}
-        >
-          <Text style={styles.createAccount_Text_Style}>Create account</Text>
-          <ArrowBlue />
-        </Pressable>
-      </View>
-    </View>
+    </SafeAreaView>
   );
 };
